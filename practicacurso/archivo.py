b57fafaa198837fe7b422182281e0405de6f8291
kevinchuca <Diego_dmp15@gmail.com>
2019-06-21 10:47:37 -0400

    gg

--- practicacurso/archivo.py
+++ practicacurso/archivo.py
@@ -4,6 +4,10 @@ from PIL import Image
 imagen = Image.open("IMG_20190620_212744.png")
 imagen.show()
 print imagen.size
+ext = imagen.resize((20, 20), Image.BICUBIC)
+#key.save("BICUBIC" + ext)
+ext.show()
+print imagen.size
 
 
 miniatura = (20, 20)
