84999cd321f902d7ebc95320de869011abcae5bd
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-31 03:10:59 +0800

    Delete practicacurso directory

--- practicacurso/archivo.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import cv2
-
-gray = cv2.imread('inumero.jpg', cv2.IMREAD_GRAYSCALE)
-t, dst = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY)
-cv2.imshow('umbral', gray)
-cv2.imshow('result', dst)
-cv2.waitKey(0)
